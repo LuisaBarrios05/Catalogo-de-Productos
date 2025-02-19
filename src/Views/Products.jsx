ee0b62fd55b0c7c0b7c90d09b2991c6a176ddfbc
Luisa Barrios <luisabarrios01@gmail.com>
2025-02-19 09:07:32 -0300

    sidebar para sección admin

--- src/Views/Products.jsx
+++ src/Views/Products.jsx
@@ -2,8 +2,11 @@ import useProductManagement from '../components/ProductManagement';
 import CardProduct from '../components/CardProduct';
 import Navbar from '../components/Navbar';
 import { Pagination } from '../components/pagination';
+import Footer from '../components/footer';
+import { SidebarWithBurgerMenu } from '../components/adminProduct/sidebar'
 
-const ProductsView = () => {
+
+const ProductsView = ({ isAdmin = false }) => {
   const {
     currentProducts,
     categories,
@@ -19,10 +22,17 @@ const ProductsView = () => {
 
   return (
     <div className="min-h-screen bg-gray-50">
+      {isAdmin && (
+        <div className="bg-gray-900 text-white py-2 font-semibold flex items-center justify-between px-4">
+          <SidebarWithBurgerMenu />
+          <p className="text-center flex-grow">Panel de administración</p>
+        </div>
+      )}
       <Navbar
         categories={categories}
         onSearch={searchProducts}
         onFilterCategory={filterByCategory}
+        isAdmin={isAdmin}
       />
 
       <div className="p-4 md:px-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
@@ -32,7 +42,7 @@ const ProductsView = () => {
             product={product}
             onEdit={handleEditProduct}
             onDelete={handleDeleteProduct}
-          //isAdmin={false}
+            isAdmin={isAdmin}
           />
         ))}
       </div>
@@ -45,6 +55,7 @@ const ProductsView = () => {
           totalPages={totalPages}
         />
       </div>
+      <Footer />
     </div>
   );
 };
